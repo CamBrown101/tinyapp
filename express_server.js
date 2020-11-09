eeb4f848500b14873056912e9cb57ed169bea99c
cambrown101 <cameron-brown@live.com>
2020-11-09 13:52:31 -0800

    added a path for /hello and added html to it.

--- express_server.js
+++ express_server.js
@@ -3,16 +3,20 @@ const app = express();
 const PORT = 8080 //default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
